42ace7868b62b6fb70a3cda1625124ca57ab4b21
leonid <kotovpetia@gmail.com>
2022-07-14 11:15:30 +0300

    initial citilink service

---
+++ src/citilink-parser-service/src/main/java/com/storeparsers/microservices/citilinkparserservice/CitilinkParserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.storeparsers.microservices.citilinkparserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitilinkParserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitilinkParserServiceApplication.class, args);
+    }
+
+}
---
+++ src/citilink-parser-service/src/test/java/com/storeparsers/microservices/citilinkparserservice/CitilinkParserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storeparsers.microservices.citilinkparserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitilinkParserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
